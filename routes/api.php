665b13d77a54267620cd0009111e398152f920b2
JuanjoRuiz <juanjose.ruiz.munoz@gmail.com>
2025-04-16 12:25:35 +0200

    Init Commit

---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::prefix('admin/user')->group(base_path('src/admin/user/infrastructure/routes/api.php'));
